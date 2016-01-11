26b319fb1d443efef230dad6bd2ef954e120b37a
Chris Thoburn <runspired@gmail.com>
2016-01-11 02:26:07 -0600

    feat(animations): adds label support, jquery support, and css-plugin support

--- addon/lib/animate.js
+++ addon/lib/animate.js
@@ -1,12 +1,12 @@
-import RSVP from 'rsvp';
+import Ember from 'ember';
 import TweenLite from 'tweenlite';
-import tween from './tween.js';
+import tween from './tween';
 
 const {
   Promise // jshint ignore:line
-} = RSVP;
+} = Ember.RSVP;
 
-export function animate(element, properties, options) {
+export function animate(element, properties /*, options, label*/) {
   if (!element || !properties) {
     return Promise.resolve();
   }
@@ -14,28 +14,39 @@ export function animate(element, properties, options) {
   return tween(...arguments);
 }
 
-export function isAnimating(element) {
-  return element && TweenLite.getTweensOf(element).length > 0;
+export function isAnimating($element, label) {
+  return getTween($element, label);
 }
 
-export function getTween(element) {
-  if (!element) {
+export function getTween($element, label) {
+  if (!$element) {
     return false;
   }
+  const element = $element.get(0);
   const tweens = TweenLite.getTweensOf(element);
-  return tweens.length > 0 ? tweens[0] : false;
+  for (let i = 0; i < tweens.length; i++) {
+    if (tweens[i].label === label) {
+      return tweens[i];
+    }
+  }
+  return false;
 }
 
 
-export function stop(element) {
-  const tween = getTween(element);
+export function stop($element, label) {
+  const tween = getTween($element, label);
   if (tween) {
     tween.kill();
     tween._deferred.reject();
   }
 }
 
-export function finish(element) {
-  const tween = getTween(element);
+export function finish($element, label) {
+  const tween = getTween($element, label);
   return tween ? tween._deferred.promise : Promise.resolve();
+}
+
+export function timeSpent($element, label) {
+  const tween = getTween($element, label);
+  return tween ? tween.progress() * tween._duration * 1000 : 0;
 }
--- addon/lib/tween.js
+++ addon/lib/tween.js
@@ -1,48 +1,59 @@
 import TweenLite from 'tweenlite';
-import RSVP from 'rsvp';
+import Ember from 'ember';
 
 const {
-  Promise // jshint ignore:line
-} = RSVP;
+  defer
+} = Ember.RSVP;
 
 const TWEEN_DEFAULTS = {
   duration: 350,
   delay: 0
 };
 
-export default function tween(element, properties, options) {
+let TWEEN_ID = 0;
+
+export default function tween($element, properties, options, label) {
+  const element = $element.get(0);
   const opts = normalizeOptions(properties, options);
+  const transition_id = '-lf-tweenlite:' + label + ':' + (TWEEN_ID++);
 
-  opts.deferred = Promise.defer();
-  opts.onComplete = opts.deferred.resolve;
+  const deferred = defer(transition_id);
+  opts.onComplete = function() { deferred.resolve(); };
   opts.onStart = function() {
-    // By default, we clear the element's `display`
-    // and `visibility` properties at the start of animation. Our
-    // animated divs are all initially rendered with `display:none`
-    // and `visibility:hidden` to prevent a flash of before-animated
-    // content.
-    if (typeof(options.display) === 'undefined') {
-      element.style.display = '';
-    }
-    if (typeof(options.visibility) === 'undefined') {
-      element.style.visibility = '';
-    }
+    element.style.display = opts.css.display;
+    element.style.visibility = opts.css.visibility;
   };
-
   const Tween = TweenLite.to(
     element,
     opts.duration,
     opts);
 
-  Tween._deferred = opts.deferred;
+  Tween.transition_id = transition_id;
+  Tween.transition_label = label;
+  Tween._deferred = deferred;
 
-  return opts.deferred.promise;
+  // debugger;
+
+  return deferred.promise;
 };
 
 function normalizeOptions(properties, options) {
-  const opts = Object.assign({}, TWEEN_DEFAULTS, properties, options);
+  const opts = Object.assign({}, TWEEN_DEFAULTS, options);
+  opts.css = properties;
   opts.duration = opts.duration / 1000;
   opts.delay = opts.delay / 1000;
+
+  // By default, we clear the element's `display`
+  // and `visibility` properties at the start of animation. Our
+  // animated divs are all initially rendered with `display:none`
+  // and `visibility:hidden` to prevent a flash of before-animated
+  // content.
+  if (typeof(opts.css.display) === 'undefined') {
+    opts.css.display = '';
+  }
+  if (typeof(opts.css.visibility) === 'undefined') {
+    opts.css.visibility = '';
+  }
 
   return opts;
 }
