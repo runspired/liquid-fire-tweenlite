4d2969aad052ccbcb0a24e0d7170f53717e43eb9
Chris Thoburn <runspired@gmail.com>
2016-01-12 12:46:30 -0600

    fix(scale-transition): corrects order of operations

--- addon/transitions/scale.js
+++ addon/transitions/scale.js
@@ -2,11 +2,11 @@ import { TransitionPromise, animate } from 'liquid-fire-tweenlite';
 
 export default function scale(opts={}) {
   return new TransitionPromise({
-    animateIn: function() {
-      return animate(this.oldElement, {scale: 0.2}, opts);
-    }.bind(this),
     animateOut: function() {
-      return animate(this.newElement, {scale: 1}, opts);
+      return animate(this.oldElement, { scale: [1, 0.2] }, opts, 'scale--out');
+    }.bind(this),
+    animateIn: function() {
+      return animate(this.newElement, { scale: [0.2, 1] }, opts, 'scale--in');
     }.bind(this)
   });
 }
