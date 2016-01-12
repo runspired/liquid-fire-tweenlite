42ebf30d9ba62f5dbc52a1dbc4cfba696cbdfa99
Chris Thoburn <runspired@gmail.com>
2016-01-12 16:05:59 -0600

    fix(flex-grow): fixes oop

--- addon/transitions/flex-grow.js
+++ addon/transitions/flex-grow.js
@@ -3,11 +3,11 @@ import { animate, stop, TransitionPromise } from 'liquid-fire-tweenlite';
 export default function flexGrow(opts) {
   stop(this.oldElement);
   return new TransitionPromise({
-    animateIn: function() {
+    animateOut: function() {
       return animate(this.oldElement, {'flex-grow': 0 }, opts);
     }.bind(this),
-    animateOut: function() {
-      return  animate(this.newElement, {'flex-grow': 1 }, opts);
+    animateIn: function() {
+      return  animate(this.newElement, {'flex-grow': [1, 0] }, opts);
     }.bind(this)
   });
 }
